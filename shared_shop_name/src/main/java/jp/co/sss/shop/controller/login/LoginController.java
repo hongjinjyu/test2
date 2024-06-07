542278d47d1d9fdf9da2d89ef06e09821aebc957
hatada0 <t.hatada09@gmail.com>
2024-06-07 15:39:37 +0900

    test commit1

--- shared_shop_name/src/main/java/jp/co/sss/shop/controller/login/LoginController.java
+++ shared_shop_name/src/main/java/jp/co/sss/shop/controller/login/LoginController.java
@@ -1,8 +1,5 @@
 package jp.co.sss.shop.controller.login;
 
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -10,6 +7,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import jp.co.sss.shop.bean.UserBean;
 import jp.co.sss.shop.form.LoginForm;
 import jp.co.sss.shop.repository.UserRepository;
