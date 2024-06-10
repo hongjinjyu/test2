7d54b32ccf5d7c43debc6cdb168f301321247206
山口菜々恵 <y.nanae.tisdx.202404@gmail.com>
2024-06-10 14:14:04 +0900

    商品一覧表示

--- shared_shop_name/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
+++ shared_shop_name/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
@@ -1,13 +1,23 @@
 package jp.co.sss.shop.controller.client.item;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.sss.shop.bean.ItemBean;
+import jp.co.sss.shop.entity.Category;
+import jp.co.sss.shop.entity.Item;
+import jp.co.sss.shop.repository.CategoryRepository;
 import jp.co.sss.shop.repository.ItemRepository;
 import jp.co.sss.shop.service.BeanTools;
+import jp.co.sss.shop.util.Constant;
 
 /**
  * 商品管理 一覧表示機能(一般会員用)のコントローラクラス
@@ -21,6 +31,47 @@ public class ClientItemShowController {
 	 */
 	@Autowired
 	ItemRepository itemRepository;
+	
+	/**
+	 * 一覧データ取得、一覧表示　処理
+	 * 
+	 * @param model Viewとの値受け渡し
+	 * @param pageable ページ制御用
+	 * @return "Client/category/list" 一覧画面 表示
+	 */
+
+	@RequestMapping(path = "/client/item/list/1", method = RequestMethod.GET)
+	public String showItemList(Model model, Pageable pageable) {
+		
+		//商品情報を全件検索(新着順)
+		//表示画面でページングが必要なため、ページ情報付きの検索を行う
+		Page<Item> itemsPage = itemRepository.findByDeleteFlagOrderByInsertDateDescPage(Constant.NOT_DELETED, pageable);
+		
+		//エンティティ内のページ情報付きの検索結果からレコードの情報だけをJavaBeansに保存
+		List<Item> itemList = itemsPage.getContent();
+		
+		//商品情報をViewへ渡す
+		model.addAttribute("pages", itemsPage);
+		model.addAttribute("items", itemList);
+		
+		return "client/item/list";
+	}
+	
+	/**
+	 * カテゴリー検索
+	 * @param categoryId
+	 * @param model
+	 * @param pageable
+	 * @return
+	 */
+	//@RequestMapping(path = "/client/item/list/{sortType}?categoryId={id}", method = RequestMethod.GET)
+	@RequestMapping(path = "/client/item/list/2", method = RequestMethod.GET)
+	public  String showId(Integer categoryId, Model model) {
+	    Category categories = new Category();
+	    categories.setId(categoryId);
+	    model.addAttribute("categories", itemRepository.findByCategory(categories));
+	    return "/";
+	}
 
 	/**
 	 * Entity、Form、Bean間のデータコピーサービス
@@ -39,4 +90,5 @@ public class ClientItemShowController {
 	
 		return "index";
 	}
+	
 }
