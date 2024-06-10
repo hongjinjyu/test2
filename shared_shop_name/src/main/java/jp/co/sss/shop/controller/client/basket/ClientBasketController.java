7c098c3f1e758d20cb51bc500318f8f98e42ba4a
喜田ひかり <h.kita.tisdx.202404@gmail.com>
2024-06-10 14:43:01 +0900

    注文かご追加、一覧表示機能を実装

--- shared_shop_name/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
+++ shared_shop_name/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
@@ -52,39 +52,45 @@ public class ClientBasketController {
 				boolean existItemInBasket=false;
 				BasketBean basketAddList = null;
 				Item item = itemRepository.getReferenceById(id);
+				Integer stock = item.getStock();
 				
-				//basketに追加したい商品が存在する場合、注文数を増やす
-				for (BasketBean basketAdd : basketList) {
-					if (basketAdd.getId() == id) {
-						basketAddList = basketAdd;
-						int newOrderNum=basketAddList.getOrderNum()+1;
-						basketAddList.setOrderNum(newOrderNum);
-						existItemInBasket=true;
+				
+				if(stock > 0) {
+					//basketに追加したい商品が存在する場合、注文数を増やす
+					for (BasketBean basketAdd : basketList) {
+						if (basketAdd.getId() == id) {
+							basketAddList = basketAdd;
+							int newOrderNum=basketAddList.getOrderNum()+1;
+							basketAddList.setOrderNum(newOrderNum);
+							existItemInBasket=true;
+						} 
+
+						//追加したい商品の個数より在庫が少ない場合
+						if(basketAddList.getOrderNum() > item.getStock()) {
+							model.addAttribute("itemNameListLessThan", item.getName());
+						}
+						
+					}
+					
+					
+					//basketに追加したい商品が存在しない場合
+					if (!existItemInBasket) {
+						basketAddList=new BasketBean();
+						basketAddList.setId(item.getId());
+						basketAddList.setName(item.getName());
+						basketAddList.setStock(item.getStock());
+						int orderNum = 1;
+						basketAddList.setOrderNum(orderNum);
+						//買い物かごリストに追加
+						basketList.add(basketAddList);
 					} 
-				}
-				
-				
-				//basketに追加したい商品が存在しない場合
-				if (!existItemInBasket) {
-					basketAddList=new BasketBean();
-					basketAddList.setId(item.getId());
-					basketAddList.setName(item.getName());
-					basketAddList.setStock(item.getStock());
-					int orderNum = 1;
-					basketAddList.setOrderNum(orderNum);
-					//買い物かごリストに追加
-					basketList.add(basketAddList);
-				} 
-				
-				//追加したい商品の在庫がない場合
-				if(item.getStock() == null) {
-					model.addAttribute("itemNameListZero", item.getName());
-				}
-				
-				//追加したい商品の個数より在庫が少ない場合
-				if(basketAddList.getOrderNum() > item.getStock()) {
-					model.addAttribute("itemNameListLessThan", item.getName());
-				}
+					
+					
+				}else
+					//追加したい商品の在庫がない場合
+					if(stock == 0) {
+						model.addAttribute("itemNameListZero", item.getName());
+					}
 				
 				//セッションに買い物かごを追加
 				session.setAttribute("basketBean", basketList);
@@ -94,37 +100,36 @@ public class ClientBasketController {
 		return "/client/basket/list";
 	}
 	
-	
-	/**
-	 * 削除ボタン 押下
-	 * @param model
-	 * @param id
-	 * @return 該当商品削除後の買い物かご一覧表示
-	 */
-	
-	/**
-	@RequestMapping(path = " /client/basket/delete/{id}", method=RequestMethod.POST)
-	public String basketDelete(Model model, Integer id) {
-		
-		BasketBean basketDelete = (BasketBean) session.getAttribute("basketBean");
-		int orderNum = basketDelete.getOrderNum();
-		
-		//該当商品の注文数が1の時
-		if(orderNum == 1) {
-			String deleteId = id.toString();
-			session.removeAttribute(deleteId);
-		
-		}else
-			//該当商品の注文数が2以上の場合
-			if(orderNum >= 2) {
-				
-				basketAddList.setOrderNum(newOrderNum);
-			}
-		return "/client/basket/list";
-	}
-	**/
-	
-	
+//
+//	
+//	/**
+//	 * 削除ボタン 押下
+//	 * @param model
+//	 * @param id
+//	 * @return 該当商品削除後の買い物かご一覧表示
+//	 */
+//	
+//	@RequestMapping(path = " /client/basket/delete/{id}", method=RequestMethod.POST)
+//	public String basketDelete(Model model, Integer id) {
+//		
+//		BasketBean basketDelete = (BasketBean) session.getAttribute("basketBean");
+//		int orderNum = basketDelete.getOrderNum();
+//		
+//		//該当商品の注文数が1の時
+//		if(orderNum == 1) {
+//			String deleteId = id.toString();
+//			session.removeAttribute(deleteId);
+//		
+//		}else
+//			//該当商品の注文数が2以上の場合
+//			if(orderNum >= 2) {
+//				
+//				basketAddList.setOrderNum(newOrderNum);
+//			}
+//		return "/client/basket/list";
+//	}
+//	
+
 	/**
 	 * 買い物かごを空にするボタン 押下
 	 * @param model
