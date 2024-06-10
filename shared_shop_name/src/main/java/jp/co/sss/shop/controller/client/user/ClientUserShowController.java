507b81309e601548fb51ee10a2ccecf63edb2596
hongjinjyu <ji.jonghyun.tisdx.202404@gmail.com>
2024-06-10 14:02:13 +0900

    詳細表示を実装しました。

---
+++ shared_shop_name/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserShowController.java
@@ -0,0 +1,69 @@
+package jp.co.sss.shop.controller.client.user;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+import jp.co.sss.shop.bean.UserBean;
+import jp.co.sss.shop.repository.UserRepository;
+
+/**
+ * 会員管理 表示機能(運用管理者、システム管理者)のコントローラクラス
+ *
+ * @author SystemShared
+ */
+@Controller
+public class ClientUserShowController {
+	/**
+	 * 会員情報　リポジトリ
+	 */
+	@Autowired
+	UserRepository userRepository;
+
+	/**
+	 * セッション情報
+	 */
+	@Autowired
+	HttpSession session;
+
+
+	/**
+	 * 詳細表示処理
+	 *
+	 * @param id 表示対象会員ID
+	 * @param model Viewとの値受渡し
+	 * @return "client/user/detail" 会員詳細表示画面へ
+	 * 
+	 * TIPS: 一般会員向けの会員詳細表示機能に類似した処理です。
+	 */
+	@RequestMapping(path = "/client/user/detail", method = { RequestMethod.GET, RequestMethod.POST })
+	public String showUser (Model model) {
+
+		
+		
+		UserBean userBean = (UserBean) session.getAttribute("user");
+		// 表示対象の情報を取得
+		Object user = userRepository.getReferenceById(userBean.getId());
+		
+		if (user == null) {
+			// 対象が無い場合、エラー
+			return "redirect:/syserror";
+		}
+
+		// Userエンティティの各フィールドの値をUserBeanにコピー
+		BeanUtils.copyProperties(user, userBean);
+
+		// 会員情報をViewに渡す
+		model.addAttribute("userBean", userBean);
+
+		//会員登録・変更・削除用のセッションスコープを初期化
+		session.removeAttribute("userForm");
+
+		// 詳細画面　表示
+		return "client/user/detail";
+	}
+}
