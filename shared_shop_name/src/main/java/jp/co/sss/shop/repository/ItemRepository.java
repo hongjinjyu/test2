d1f379e8c12d1a4ea6129b0fc464332f26928a66
hongjinjyu <ji.jonghyun.tisdx.202404@gmail.com>
2024-06-20 13:51:05 +0900

    売れたことない商品も売れ筋順に出るようになりました。

--- shared_shop_name/src/main/java/jp/co/sss/shop/repository/ItemRepository.java
+++ shared_shop_name/src/main/java/jp/co/sss/shop/repository/ItemRepository.java
@@ -56,22 +56,37 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
     @Query("SELECT i FROM Item i INNER JOIN i.category c WHERE i.deleteFlag =:deleteFlag AND i.category=:category ORDER BY i.id DESC")
 	Page<Item> findByCategoryOrderByIdDesc(@Param(value = "deleteFlag") int deleteFlag, @Param(value="category") Category category, Pageable pageable);
 	
+//    /**
+//     * 商品全件表示(売れ筋順)
+//     */
+//    @Query("SELECT i FROM OrderItem o INNER JOIN Item i ON o. item.id=i.id WHERE i.deleteFlag =:deleteFlag GROUP BY i ORDER BY COUNT(i) DESC,i.id ASC")
+//    Page<Item> findAllByQuery(@Param(value = "deleteFlag") int deleteFlag, Pageable pageable);
+//    
+//    /**
+//     * 商品全件表示(売れ筋順) トップ画面表示用
+//     */
+//    @Query("SELECT i FROM OrderItem o INNER JOIN Item i ON o. item.id=i.id WHERE i.deleteFlag =:deleteFlag GROUP BY i ORDER BY COUNT(i) DESC,i.id ASC")
+//    List<Item> findAllByQuery(@Param(value = "deleteFlag") int deleteFlag);
+//    
+    
     /**
      * 商品全件表示(売れ筋順)
      */
-    @Query("SELECT i FROM OrderItem o INNER JOIN Item i ON o. item.id=i.id WHERE i.deleteFlag =:deleteFlag GROUP BY i ORDER BY COUNT(i) DESC,i.id ASC")
+    @Query("SELECT i FROM Item i LEFT JOIN OrderItem o ON o.item.id = i.id WHERE i.deleteFlag = :deleteFlag GROUP BY i ORDER BY COALESCE(SUM(o.quantity), 0) DESC, i.id ASC")
+
     Page<Item> findAllByQuery(@Param(value = "deleteFlag") int deleteFlag, Pageable pageable);
     
     /**
      * 商品全件表示(売れ筋順) トップ画面表示用
      */
-    @Query("SELECT i FROM OrderItem o INNER JOIN Item i ON o. item.id=i.id WHERE i.deleteFlag =:deleteFlag GROUP BY i ORDER BY COUNT(i) DESC,i.id ASC")
+    @Query("SELECT i FROM Item i LEFT JOIN OrderItem o ON o.item.id = i.id WHERE i.deleteFlag = :deleteFlag GROUP BY i ORDER BY COALESCE(SUM(o.quantity), 0) DESC, i.id ASC")
+
     List<Item> findAllByQuery(@Param(value = "deleteFlag") int deleteFlag);
     
     /**
      * 商品全件表示(売れ筋順かつカテゴリで絞り込み)
      */
-    @Query("SELECT i FROM OrderItem o INNER JOIN Item i ON o.item.id=i.id WHERE i.deleteFlag =:deleteFlag AND i.category.id=:categoryId GROUP BY i ORDER BY COUNT(i) DESC, i.id ASC")
+    @Query("SELECT i FROM Item i LEFT JOIN OrderItem o ON o.item.id=i.id WHERE i.deleteFlag = :deleteFlag AND i.category.id=:categoryId GROUP BY i ORDER BY COALESCE(SUM(o.quantity),0) DESC, i.id ASC")
     Page<Item> findCategoryByQuery(@Param(value = "deleteFlag") int deleteFlag, @Param(value="categoryId")Integer categoryId, Pageable pageable);
     
 	 /**
